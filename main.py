72b084120856523bc4a94dd4ffacce0f3c873d44
sebastianArango99 <137566308+sebastianArango99@users.noreply.github.com>
2024-05-19 20:23:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     """Example Hello World route."""
-    name = os.environ.get("NAME", "World")
+    name = os.environ.get("NAME", "World, Experto en GCP")
     return f"Hello {name}!"
 
 
